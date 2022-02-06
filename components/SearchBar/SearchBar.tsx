20561932f0c3d9518c6124c331f109b314d2e5ed
fermidrates <albertfarand@gmail.com>
2022-02-06 21:37:00 +0700

    fix: onchange interaction and responsiveness

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -9,23 +9,14 @@ export const SearchBar = ({
 }: inputSearchPropsType) => {
   label="search";
 
-  const handleSearch = (e: any) => {
-    const searchKey = e.target.value;
-    return searchKey;
-  };
-
   return (
     <div className={style.input_custom}>
       <input
-        onChange={handleSearch}
+        onChange={onChange}
         type='text'
         placeholder='Enter event name or location'
         className={style.input_text}
       />
-      <span
-        className={style.search_button}>
-        <ButtonPrimary title="Search"/>
-      </span>
     </div>
   );
 };
