86263fc9a170245b2a54fa3e3c33e3e1755b0175
fermidrates <60467417+fermidrates@users.noreply.github.com>
2022-02-07 19:46:19 +0700

    fix: conflict on _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,8 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { ApolloProvider } from "@apollo/client";
-import client from "../graphql/client";
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
