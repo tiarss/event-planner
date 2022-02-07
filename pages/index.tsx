82e877f812fd6252fe9b48e43a70cc4042dbb5d8
fermidrates <albertfarand@gmail.com>
2022-02-07 12:43:53 +0700

    fix: variable naming

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import { CardsHome } from '../components/CardsHome/CardsHome';
 import { ButtonPrimary } from '../components/Button/Button';
 import { GET_MOST_ATTENDANT_EVENTS, GET_JOINABLE_EVENTS ,GET_EVENTS_BY_SEARCH } from '../graphql/Query';
 
-const Home: NextPage = () => {
+function Home() {
   // const token = localStorage.getItem('token');
   const router = useRouter();
   const limit: number = 3;
@@ -17,65 +17,68 @@ const Home: NextPage = () => {
   var currentSearchValue = "";
   const [searchValue, setSearchValue] = useState<string>("");
 
-  const {loading, error, data} = useQuery(GET_MOST_ATTENDANT_EVENTS);
+  const {loading: loadingAttendant, error: errorAttendant, data: dataAttendant} = useQuery(GET_MOST_ATTENDANT_EVENTS);
 
-  // const {loading: loadingJoinable, error: errorJoinable, data: dataJoinable} = useQuery(GET_JOINABLE_EVENTS);
+  const {loading: loadingJoinable, error: errorJoinable, data: dataJoinable} = useQuery(GET_JOINABLE_EVENTS);
   
   // const {loading, error, data} = useQuery(GET_EVENTS_BY_SEARCH, {
   //   variables: {
   //     search: searchValue
   //   }
   // });
-  if(loading) {
+
+  console.log(`attendant: ${dataAttendant}`);
+  console.log(`joinable: ${dataJoinable}`);
+
+  if(loadingAttendant && loadingJoinable) {
     return (
       <p>loading</p>
     )
-  } else if(error) {
-    return <p>error</p>
+  } else {
+    return (
+      <>
+        <div className="d-flex py-2 justify-content-center align-items-center">
+          <SearchBar onChange={(e) => currentSearchValue=e.target.value}/>
+          <span>
+            <ButtonPrimary title="Search" onClick={() => setSearchValue(currentSearchValue)}/>
+          </span>
+        </div>
+        <div className='d-flex container w-75 px-4 pt-4 pb-2'>
+          <h3>Most Attendant</h3>
+        </div>
+        <div className="d-flex container justify-content-center w-75">
+          <div className="d-flex container px-2 flex-wrap">
+            {dataAttendant.getEventMostAttendant.map((e: any) => (
+              <div key={e.id} className="p-2">
+                <CardsHome
+                  title={e.title.length > 22 ? e.title.substring(0,22) + ".." : e.title} 
+                  location={e.location}
+                  image={e.image}
+                  date={moment(e.date).format('YYYY MMM D, hh:mm ') + "WIB"}/>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='d-flex container justify-content-between align-items-center w-75 px-4 pt-4 pb-2'>
+          <h3>Join These Events</h3>
+          <h6><Link href="/search"><a>See All Events</a></Link></h6>
+        </div>
+        <div className="d-flex container justify-content-center w-75">
+          <div className="d-flex container px-2 flex-wrap">
+            {dataJoinable.getEventMostAttendant.map((e: any) => (
+              <div key={e.id} className="p-2">
+                <CardsHome
+                  title={e.title.length > 22 ? e.title.substring(0,22) + ".." : e.title} 
+                  location={e.location}
+                  image={e.image}
+                  date={moment(e.date).format('YYYY MMM D, hh:mm ') + "WIB"}/>
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    )
   }
-  return (
-    <>
-      <div className="d-flex py-2 justify-content-center align-items-center">
-        <SearchBar onChange={(e) => currentSearchValue=e.target.value}/>
-        <span>
-          <ButtonPrimary title="Search" onClick={() => setSearchValue(currentSearchValue)}/>
-        </span>
-      </div>
-      <div className='d-flex container w-75 px-4 pt-4 pb-2'>
-        <h3>Most Attendant</h3>
-      </div>
-      <div className="d-flex container justify-content-center w-75">
-        <div className="d-flex container px-2 flex-wrap">
-          {data.getEventMostAttendant.map((e: any) => (
-            <div key={e.id} className="p-2">
-              <CardsHome
-                title={e.title.length > 22 ? e.title.substring(0,22) + ".." : e.title} 
-                location={e.location}
-                image={e.image}
-                date={moment(e.date).format('YYYY MMM D, hh:mm ') + "WIB"}/>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className='d-flex container justify-content-between align-items-center w-75 px-4 pt-4 pb-2'>
-        <h3>Join These Events</h3>
-        <h6><Link href="/search"><a>See All Events</a></Link></h6>
-      </div>
-      <div className="d-flex container justify-content-center w-75">
-        <div className="d-flex container px-2 flex-wrap">
-          {data.getEventMostAttendant.map((e: any) => (
-            <div key={e.id} className="p-2">
-              <CardsHome
-                title={e.title.length > 22 ? e.title.substring(0,22) + ".." : e.title} 
-                location={e.location}
-                image={e.image}
-                date={moment(e.date).format('YYYY MMM D, hh:mm ') + "WIB"}/>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
-  )
 }
 
 export default Home;
--- pages/search.tsx
+++ pages/search.tsx
@@ -2,7 +2,7 @@ import react, { useState } from 'react';
 import type { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from 'next/router';
-import { useQuery } from '@apollo/client';
+import { useQuery, useLazyQuery } from '@apollo/client';
 import { GET_ALL_EVENTS_PAGINATE, GET_EVENTS_BY_SEARCH } from '../graphql/Query';
 import moment from 'moment';
 import { SearchBar } from '../components/SearchBar/SearchBar';
@@ -19,66 +19,65 @@ const Search: NextPage = () => {
   var currentSearchValue = "";
   const [searchValue, setSearchValue] = useState<string>("");
   
-  // const {loading, error, data} = useQuery(GET_ALL_EVENTS_PAGINATE, {
-  //   variables: {  
-  //     limit: limit,
-  //     offset: offset
-  //   }
-  // });
-  
-  const {loading, error, data} = useQuery(GET_EVENTS_BY_SEARCH, {
-    variables: {
-      search: searchValue
+  const {loading: loadingPaginate, data: dataPaginate} = useQuery(GET_ALL_EVENTS_PAGINATE, {
+    variables: {  
+      limit: limit,
+      offset: offset
     }
   });
-  if(loading) {
+  
+  const [search, {loading: loadingSearch, data: dataSearch}] = useLazyQuery(GET_EVENTS_BY_SEARCH);
+
+  console.log(`page: ${dataPaginate}`);
+  console.log(`search: ${dataSearch}`);
+
+  if(loadingSearch) {
     return (
       <p>loading</p>
     )
-  } else if(error) {
-    return <p>error</p>
-  }
-  return (
-    <>
-      <div className="d-flex py-2 justify-content-center align-items-center">
-        <SearchBar onChange={(e) => currentSearchValue=e.target.value}/>
-        <span
-          className={styles.search_button}>
-          <ButtonPrimary title="Search" onClick={() => setSearchValue(currentSearchValue)}/>
-        </span>
-      </div>
-      <div className='d-flex container w-75 px-4 pt-4 pb-2'>
-        <h3>All Events</h3>
-      </div>
-      <div className="d-flex container justify-content-center w-75">
-        <div className="d-flex container px-2 flex-wrap">
-          {data.getEventsBySearch.map((e: any) => (
-            <div key={e.id} className="p-2">
-              <CardsHome
-                title={e.title.length > 22 ? e.title.substring(0,22) + ".." : e.title} 
-                location={e.location}
-                image={e.image}
-                date={moment(e.date).format('YYYY MMM D, hh:mm ') + "WIB"}/>
+  } else {
+    return (
+      <>
+        <div className="d-flex py-2 justify-content-center align-items-center">
+          <SearchBar onChange={(e) => setSearchValue(e.target.value)}/>
+          <span
+            className={styles.search_button}>
+            <ButtonPrimary title="Search" onClick={() => search({ variables: { search: searchValue } })}/>
+          </span>
+        </div>
+        <div className='d-flex container w-75 px-4 pt-4 pb-2'>
+          <h3>All Events</h3>
+        </div>
+        <div className="d-flex container justify-content-center w-75">
+          <div className="d-flex container px-2 flex-wrap">
+            {/* {dataPaginate.getPaginationEvents.map((e: any) => (
+              <div key={e.id} className="p-2">
+                <CardsHome
+                  title={e.title.length > 22 ? e.title.substring(0,22) + ".." : e.title} 
+                  location={e.location}
+                  image={e.image}
+                  date={moment(e.date).format('YYYY MMM D, hh:mm ') + "WIB"}/>
+              </div>
+            ))} */}
+          </div>
+        </div>
+        <div className="d-flex container w-75 justify-content-end align-items-center px-5 py-2">
+          <div className="px-2">prev</div>
+          <div className={styles.prev_button} onClick={() => setOffset(offset+limit)}>
+            <div className={styles.icon_positioning}>
+              <AiOutlineLeft />
             </div>
-          ))}
-        </div>
-      </div>
-      <div className="d-flex container w-75 justify-content-end align-items-center px-5 py-2">
-        <div className="px-2">prev</div>
-        <div className={styles.prev_button} onClick={() => setOffset(offset+limit)}>
-          <div className={styles.icon_positioning}>
-            <AiOutlineLeft />
           </div>
-        </div>
-        <div className={styles.next_button} onClick={() => setOffset(offset-limit)}>
-          <div className={styles.icon_positioning}>
-            <AiOutlineRight />
+          <div className={styles.next_button} onClick={() => setOffset(offset-limit)}>
+            <div className={styles.icon_positioning}>
+              <AiOutlineRight />
+            </div>
           </div>
+          <div className="px-2">next</div>
         </div>
-        <div className="px-2">next</div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default Search;
